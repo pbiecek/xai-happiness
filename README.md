652ff720400f480bf7133f9864f73d142bb88b12
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2020-10-12 21:39:49 +0200

    happiness use case

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # xai-happiness
-XAI use case based on world-happiness data
+
+XAI use case based on world-happiness data.
+
+## What factors correlate with happiness at national level?
+
+1. Get data about happiness from 
+https://www.kaggle.com/unsdsn/world-happiness
+
+2. Train your favorite ML model that will predict the happiness score
+
+3. Explain the model with [modelStudio](https://github.com/ModelOriented/modelStudio)
+
+```
+library("DALEX")
+library("modelStudio")
+library("ranger")
+
+# get data
+happiness <- readRDS("happiness.rds")
+
+# get model and explainer
+model <- ranger(score~., data = happiness)
+modelr <- explain(model, data = happiness[,-1], y = happiness$score)
+
+# get model studio
+ms <- modelStudio(modelr, happiness[c("Poland","Finland","Germany"),])
+new_ms <- ms_update_options(ms, margin_left = 220)
+new_ms
+```
+
---
+++ modelstudio.R
@@ -0,0 +1,39 @@
+library("dplyr")
+library("ggplot2")
+library("patchwork")
+library("ranger")
+library("xgboost")
+library("DALEX")
+library("rms")
+library("modelStudio")
+
+# Get the data from 
+# https://www.kaggle.com/unsdsn/world-happiness#
+happiness <- read.table("2019.csv", sep = ",", header = TRUE)
+
+row.names(happiness) <- happiness[,2]
+happiness <- happiness[,-(1:2)]
+
+col_names <- names(happiness)
+col_names <- tolower(col_names)
+col_names <- lapply(strsplit(col_names, "[.]"), paste, collapse  ="_")
+colnames(happiness) <- col_names
+
+saveRDS(happiness, file = "happiness.rds")
+
+happiness <- readRDS("happiness.rds")
+
+model <- ranger(score~., data = happiness)
+modelr <- DALEX::explain(model, data = happiness[,-1], y = happiness$score)
+
+# small
+ms <- modelStudio(modelr, happiness[c("Poland","Finland","Germany"),])
+new_ms <- ms_update_options(ms, margin_left = 220)
+new_ms
+
+# large
+ms <- modelStudio(modelr, happiness)
+new_ms <- ms_update_options(ms, margin_left = 220, ms_title = "What makes you happy? (country level)")
+new_ms
+
+r2d3::save_d3_html(new_ms, file = "index.html")
